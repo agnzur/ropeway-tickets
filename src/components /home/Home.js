894d16fd5c61846954cc1bb9b9b77f4999b5c60a
Agnieszka <aagnieszka.zurek@gmail.com>
2023-03-03 17:30:17 +0100

    minor ref.

--- src/components /home/Home.js
+++ src/components /home/Home.js
@@ -7,15 +7,15 @@ export const Home = () => {
         <GoToShopButton />
       </div>
       <div className="text-container">
-        Kolejka Radość to atrakcja działąjąca w ramach Kolei Linowych. To
+        Kolejka Radość to atrakcja działająca w ramach Kolei Linowych. To
         doskonała całoroczna forma aktywnego spędzenia czasu w otoczeniu natury,
         z dala od codziennego zgiełku. Kolejka pokonuje trasę o długości 3km i
         wznosi się od 800 m n.p.m. na stacji początkowej do 1800 m n.p.m. na
         stacji końcowej. Trasa biegnie przez las aż do malowniczego łańcucha
-        górskiego. Pasażerowie w trakcie podróży kolejką mają okazję do
-        obcowania z sielksim krajobrazem, ale także do obserwowania życia
-        bogatej fauny i flory. Wśród mieszkańców trasy można wyróżnić m.in.
-        płochacze halne, orły przednie, świstaki, rysie oraz lisy.
+        górskiego. Pasażerowie w trakcie podróży mają okazję do obcowania z
+        sielksim krajobrazem, a także do obserwowania życia bogatej fauny i
+        flory. Wśród mieszkańców trasy można wyróżnić m.in. płochacze halne,
+        orły przednie, świstaki, rysie oraz lisy.
       </div>
     </div>
   );
