8a976f1ebb211f69fcecbdca62e92086b0ff922b
Agnieszka <aagnieszka.zurek@gmail.com>
2023-03-05 14:22:23 +0100

    ticket price holder added

--- src/components /booking/TicketsGrid.js
+++ src/components /booking/TicketsGrid.js
@@ -2,14 +2,20 @@ export const TicketsGrid = () => {
   return (
     <div className="tickets-container">
       <div className="ticket-widget">
-        <div className="ticket-type">Grupowy dla 6 osób w jedną stronę</div>
+        <div className="ticket-type">
+          Grupowy dla 6 osób w jedną stronę
+          <div className="ticket-price"> zł</div>
+        </div>
         <div className="add-button-container">
           <button className="descrease-button add-button">-</button>0
           <button className="increase-button add-button">+</button>
         </div>
       </div>
       <div className="ticket-widget">
-        <div className="ticket-type">Grupowy dla 6 osób w jedną stronę</div>
+        <div className="ticket-type">
+          Grupowy dla 6 osób w jedną stronę
+          <div className="ticket-price"> zł</div>
+        </div>
         <div className="add-button-container">
           <button className="descrease-button add-button">-</button>0
           <button className="increase-button add-button">+</button>
