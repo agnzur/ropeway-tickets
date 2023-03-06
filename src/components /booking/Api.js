7f13d99682043b6e449815d7d5dda5edf50aab65
Agnieszka <aagnieszka.zurek@gmail.com>
2023-03-06 11:13:14 +0100

    api data correction

--- src/components /booking/Api.js
+++ src/components /booking/Api.js
@@ -11,19 +11,13 @@ export async function getAvailableTickets() {
       name: "Ulgowy w dwie strony",
       price: 48,
     },
-    { id: 4, name: "Ulgowy w dwie strony", price: 24 },
+    { id: 4, name: "Ulgowy w jedną strony", price: 24 },
     {
       id: 5,
-      name: "Grupowy dla 6 osób w jedną stronę",
+      name: "Grupowy dla 6 osób dwie strony",
       price: 300,
     },
-    { id: 6, name: "Ulgowy w dwie strony", price: 24 },
-    {
-      id: 7,
-      name: "Grupowy dla 6 osób w dwie strony",
-      price: 300,
-    },
-    { id: 8, name: "Grupowy dla 6 vosób w jedną stronę", price: 150 },
+    { id: 6, name: "Grupowy dla 6 vosób w jedną stronę", price: 150 },
   ];
   return availableTickets;
 }
