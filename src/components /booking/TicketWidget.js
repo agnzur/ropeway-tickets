8a710e04b753ab05c15e148f6256b4609b4f2778
Agnieszka <aagnieszka.zurek@gmail.com>
2023-03-07 16:22:24 +0100

    fixed bug when adding more than one ticket

--- src/components /booking/TicketWidget.js
+++ src/components /booking/TicketWidget.js
@@ -2,15 +2,25 @@ export const TicketWidget = ({ id, name, price, setOrder, order }) => {
   const quantity = order.find((x) => x.id === id)?.quantity ?? 0;
 
   const addTicket = () => {
-    let newOrder = [
-      ...order,
-      {
-        id: id,
-        name: name,
-        price: price,
-        quantity: quantity + 1,
-      },
-    ];
+    let newOrder;
+    if (quantity === 0) {
+      newOrder = [
+        ...order,
+        {
+          id: id,
+          name: name,
+          price: price,
+          quantity: quantity + 1,
+        },
+      ];
+    } else {
+      newOrder = order.map((x) => {
+        if (x.id === id) {
+          return { id: id, name: name, price: price, quantity: x.quantity + 1 };
+        }
+        return x;
+      });
+    }
     setOrder(newOrder);
   };
 
