53d38136a765d73cc555e25ab0509875f404c192
Agnieszka <aagnieszka.zurek@gmail.com>
2023-03-09 20:18:52 +0100

    inputs grid styling

--- src/components /summary/Inputs.js
+++ src/components /summary/Inputs.js
@@ -9,29 +9,29 @@ export const Inputs = ({ email, setEmail, date, setDate }) => {
   };
 
   return (
-    <div>
-      <div className="input-container">
-        <label htmlFor="email-input">Wpisz swój e-mail</label>
-        <input
-          id="email-input"
-          className="client-input"
-          placeholder="e-mail"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        ></input>
-      </div>
-      <div className="input-container">
-        <label htmlFor="date-input">Wybierz datę rezerwacji</label>
-        <input
-          id="date-input"
-          className="client-input"
-          type="date"
-          value={newDate}
-          onChange={handleDateChange}
-        ></input>
-      </div>
+    <div className="grid-container">
+      <label htmlFor="email-input" className="input-label">
+        Wpisz swój e-mail
+      </label>
+      <input
+        id="email-input"
+        className="client-input"
+        placeholder="e-mail"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      ></input>
+      <label htmlFor="date-input" className="input-label">
+        Wybierz datę rezerwacji
+      </label>
+      <input
+        id="date-input"
+        className="client-input"
+        type="date"
+        value={newDate}
+        onChange={handleDateChange}
+      ></input>
     </div>
   );
 };
